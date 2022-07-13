54c48ad13b3ee434f5ade04d1556030342a33e48
Alberto Klein <klein994@hotmail.com>
2022-07-13 00:02:44 -0300

    first

---
+++ controllers/webController.js
@@ -0,0 +1,16 @@
+const webControllers = {
+    inicio: (req, res) => {
+        res.sendFile('index.html', { root: './public/views' })
+    },
+    login: (req, res) => {
+        res.sendFile('login.html', { root: './public/views' })
+    },
+    logout: (req, res) => {
+        res.sendFile('logout.html', { root: './public/views' })
+    },
+    registrar: (req, res) => {
+        res.sendFile('registrar.html', { root: './public/views' })
+    }
+}
+
+export default webControllers;
